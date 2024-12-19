dc258265de048d9d67ee38cfad1a9bffcc6f29d0
gulnurbugday <glnrbgday@gmail.com>
2024-12-19 14:37:25 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -9,15 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
             star.addEventListener("click", () => {
                 const rating = star.dataset.value;
 
-                // Tüm yıldızların seçimini temizle
                 stars.forEach((s) => s.classList.remove("selected"));
 
-                // Seçilen yıldızlara kadar işaretle
                 for (let i = 0; i < rating; i++) {
                     stars[i].classList.add("selected");
                 }
 
-                // Sonucu göster
                 result.textContent = `${rating}/5 Yıldız olarak oy verdiniz.`;
             });
         });
